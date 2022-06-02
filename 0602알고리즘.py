df8afb35bcbdead2408e0d27aa9ca28549cfeb4a
김준호 <105583914+KimJunHo15@users.noreply.github.com>
2022-06-02 21:40:54 +0900

    0602알고리즘 (백준2163번)

---
+++ 0602알고리즘.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[20]:
+
+
+def choco(num1,num2) :
+#     num1 = int(input())
+#     num2 = int(input())
+    return (num1-1) + num1*(num2-1)
+
+
+# In[25]:
+
+
+choco(5,3)
+
